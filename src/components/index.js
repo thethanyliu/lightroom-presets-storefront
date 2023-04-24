2301a103b5f6deca1adaf6e64ff5eeadbf9fd84f
Ethan <62630748+ethanliu666@users.noreply.github.com>
2023-04-24 09:56:07 -0600

    added components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Footer } from "./Footer"
+export { default as Cart } from "./Cart"
+export { default as Banner } from "./Banner"
+export { default as Layout } from "./Layout"
+export { default as Navbar } from "./Navbar"
+export { default as Product } from "./Product"
