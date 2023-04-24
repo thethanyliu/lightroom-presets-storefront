09d817bc9fdc43f47eb9b0b34f2f71d39f288d86
Ethan <62630748+ethanliu666@users.noreply.github.com>
2023-04-24 15:58:09 -0600

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="bg-transparent w-screen h-16 z-20 fixed">
+      <div className="px-3 justify-between items-center w-full h-full flex border-b">
+        <ul className="hidden md:flex">
+          <li className="pl-8 cursor-pointer">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="pl-5 cursor-pointer">
+            <Link href="/products">Products</Link>
+          </li>
+          <li className="pl-5 cursor-pointer">
+            <Link href="/tutorial">Tutorial</Link>
+          </li>
+          <li className="pl-5 cursor-pointer">
+            <Link href="/contact-us">Contact</Link>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
