e3f05e73f90316f413b3341886e9ab20dde82825
Ethan <62630748+ethanliu666@users.noreply.github.com>
2023-05-01 18:40:02 -0600

    updated context

--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -1,4 +1,18 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
 
-const Context = createContext() 
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQty, setTotalQty] = useState();
+  const [qty, setQty] = useState(1);
+
+  return (
+    <Context.Provider value={(showCart, cartItems, totalPrice, totalQty, qty)}>
+      {children}
+    </Context.Provider>
+  );
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,17 @@
 import "@/styles/globals.css";
 import React from "react";
 import { Layout } from "@/components";
+import { StateContext } from "@/context/StateContext";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
   // the "Component" component refers to the current page
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
