3d2a9282e6bd5cdd9a3ad486586cc4fddf432c92
Ethan <62630748+ethanliu666@users.noreply.github.com>
2023-05-06 16:25:09 -0600

    added stripe

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// API folder servers as the backend of application, not rendered on the frontend
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
---
+++ src/pages/api/stripe.js
@@ -0,0 +1,33 @@
+const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      // Create Checkout Sessions from body params.
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: "{{PRICE_ID}}",
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+        automatic_tax: { enabled: true },
+      };
+      const session = await stripe.checkout.sessions.create(params);
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
