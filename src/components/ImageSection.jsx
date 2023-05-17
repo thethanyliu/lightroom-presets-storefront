84068d871bb291ed13d0a62d2d2db7f66a9c2bc4
Ethan Liu <62630748+ethanliu666@users.noreply.github.com>
2023-05-16 20:56:36 -0600

    updated homepage

--- src/components/ImageSection.jsx
+++ src/components/ImageSection.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const ImageSection = () => {
   return (
     <div className='image-section-container'>
-      
+      <div>
+        Upgrading your photos, just got easier.
+      </div>
+      <div className='image-section-image'>
+
+      </div>
     </div>
   )
 }
