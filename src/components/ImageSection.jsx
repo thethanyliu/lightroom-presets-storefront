75f1a4d1745cae83e3fe10eda63679033be20982
Ethan Liu <62630748+ethanliu666@users.noreply.github.com>
2023-05-29 09:05:07 -0600

    updated css

--- src/components/ImageSection.jsx
+++ src/components/ImageSection.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import classes from "./ImageSection.module.css"
+import { Slider } from '.'
 
 const ImageSection = () => {
   return (
     <div className={classes.imageSectionContainer}>
-      <h1>
-        Upgrading your photo game, just got easier.
-      </h1>
+      <div className={classes.sliderContainer}>
+        <Slider beforeImage={"./b.jpg"} afterImage={"./a.jpg"}/>
+        <p>Vintage Tones Preset Bundle - Neon Glow</p>
+      </div>
+      
       <div className='image-section-image'>
 
       </div>
