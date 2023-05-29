8f30e95b65aaec025c15991c589e49ed8d0f0a3e
Ethan Liu <62630748+ethanliu666@users.noreply.github.com>
2023-05-29 16:33:07 -0600

    updated components

--- src/components/ImageSection.jsx
+++ src/components/ImageSection.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
-import classes from "./ImageSection.module.css"
-import { Slider } from '.'
+import React from "react";
+import classes from "./ImageSection.module.css";
+import { Slider } from ".";
+import Link from "next/link";
 
 const ImageSection = () => {
   return (
     <div className={classes.imageSectionContainer}>
       <div className={classes.sliderContainer}>
-        <Slider beforeImage={"./b.jpg"} afterImage={"./a.jpg"}/>
+        <div className={classes.sliderInnerContainer}>
+          <Slider beforeImage={"./b.jpg"} afterImage={"./a.jpg"} />
+        </div>
+        
         <p>Vintage Tones Preset Bundle - Neon Glow</p>
       </div>
-      
-      <div className='image-section-image'>
-
+      <div className={classes.imageSectionImage}>
+        <div className={classes.firstInnerImage}>
+          <h2>Lightroom Presets</h2>
+          <button type="button" className={classes.shopButton}>
+            <Link
+              style={{ textDecoration: "none", color: "black", fontWeight: 600 }}
+              href="/products"
+            >
+              Shop Now
+            </Link>
+          </button>
+        </div>
+        <div>another image</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ImageSection
+export default ImageSection;
