07d0fa057e071f46ebe9da51af25b487dd508085
Ethan Liu <62630748+ethanliu666@users.noreply.github.com>
2023-05-30 15:07:58 -0600

    updated content

--- src/components/StatsSection.jsx
+++ src/components/StatsSection.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import classes from "./StatsSection.module.css";
+import Image from "next/image";
 
 const StatsSection = () => {
   return (
     <div className={classes.container}>
-      <div></div>
-      <div>
+      <div className={classes.imageContainer}>
+        <Image
+          src="/mobile_desktop.png"
+          alt="desktop and mobile image"
+          width={500}
+          height={350}
+        ></Image>
+      </div>
+      <div className={classes.textContainer}>
         <h1 className={classes.title}>
-          Upgrading your photo game has never been easier.
+          Mobile + Desktop Presets
         </h1>
         <p className={classes.text}>
-          With over 40+ Lightroom mobile (XMP files) and Lightroom desktop
+          Upgrading your photo game has never been easier. With over 40+ Lightroom mobile (XMP files) and Lightroom desktop
           presets (DNG files) in our Master Collection, each designed to enhance
-          your creative workflow, you can be sure that there will be a preset
-          to make every photo look spectacular.
+          your creative workflow, you can be sure that there will be a preset to
+          make every photo look spectacular.
         </p>
       </div>
     </div>
