44e41af7109f9eb1dc5fa08f8097bea33f932bf0
Ethan Liu <62630748+ethanliu666@users.noreply.github.com>
2023-05-31 12:09:01 -0600

    updated stats section

--- src/components/PresetStatsSection.jsx
+++ src/components/PresetStatsSection.jsx
@@ -4,11 +4,11 @@ import classes from "./PresetStatsSection.module.css"
 const PresetStatsSection = ({ presetNumber }) => {
   return (
     <div className={classes.container}>
-      <div>
+      <div className={classes.textContainer}>
         <h1>{presetNumber} Custom Presets</h1>
       </div>
       <div>
-
+        
       </div>
     </div>
   )
