7b46f506b30ce4c444e677821eebe59d3109e418
Ethan Liu <62630748+ethanliu666@users.noreply.github.com>
2023-06-14 14:17:10 -0600

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,25 +5,29 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <>
-      <div>
-        <Link
-          style={{
-            textDecoration: "none",
-            color: "grey",
-            fontWeight: 500,
-            paddingRight: 4,
-          }}
-          href="/terms-and-conditions"
-        >
-          Terms and conditons
-        </Link>{" "}
-        <Link
-          style={{ textDecoration: "none", color: "grey", fontWeight: 500 }}
-          href="/privacy-policy"
-        >
-          Privacy policy
-        </Link>
+      <div className={classes.container}>
+        <div>Instagram</div>
+        <div>
+          <Link
+            style={{
+              textDecoration: "none",
+              color: "grey",
+              fontWeight: 500,
+              paddingRight: 4,
+            }}
+            href="/terms-and-conditions"
+          >
+            Terms and conditons
+          </Link>{" "}
+          <Link
+            style={{ textDecoration: "none", color: "grey", fontWeight: 500 }}
+            href="/privacy-policy"
+          >
+            Privacy policy
+          </Link>
+        </div>
       </div>
+
       <div className={classes.footer}>
         <p>&copy; 2023 Pnutpresets All Rights Reserved</p>
       </div>
