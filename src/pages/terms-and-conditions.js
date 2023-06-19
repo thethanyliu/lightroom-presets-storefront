47556227aa453917bb19b2ca3b35a4b2269c2556
Ethan Liu <62630748+ethanliu666@users.noreply.github.com>
2023-06-19 13:44:33 -0600

    updated pages

--- src/pages/terms-and-conditions.js
+++ src/pages/terms-and-conditions.js
@@ -8,238 +8,89 @@ const termsAndConditions = () => {
         <br />
         <p>Our website address is: pnutpresets.com </p>
         <br />
-        <p>
-          Protecting your private information is our priority. This Statement of
-          Privacy applies to www.pnutpresets.com and PnutPresets and governs
-          data collection and usage. For the purposes of this Privacy Policy,
-          unless otherwise noted, all references to PnutPresets include
-          www.pnutpresets.com and PnutPresets. The PnutPresets website for
-          selling music devices & electronic headsets. By using the PnutPresets
-          website, you consent to the data practices described in this
-          statement.{" "}
-        </p>
+
+        <h2>Acceptance of Terms</h2>
         <br />
-        <h2>Collection of your Personal Information </h2>
+        <h2>Intellectual Property</h2>
         <br />
         <p>
-          In order to better provide you with products and services offered on
-          our Site, PnutPresets may collect personally identifiable information,
-          such as your:
+          All content on the Website, including but not limited to text,
+          graphics, logos, images, and software, is the property of PnutPresets
+          and is protected by applicable intellectual property laws. You may not
+          reproduce, distribute, modify, or create derivative works from any
+          content on the Website without prior written consent from PnutPresets.
         </p>
         <br />
-        <ul>
-          <li className="bullet-point">– First and Last Name</li>
-          <li className="bullet-point">– Mailing Address </li>
-          <li className="bullet-point">– E-mail Address </li>
-        </ul>
+        <h2>User Conduct</h2>
         <br />
-        <p>
-          If you purchase PnutPresets’s products and services, we collect
-          billing and credit card information. This information is used to
-          complete the purchase transaction.
-        </p>
-        <p>
-          Please keep in mind that if you directly disclose personally
-          identifiable information or personally sensitive data through
-          PnutPresets’s public message boards, this information may be collected
-          and used by others.
-        </p>
-        <br />
-        <p>
-          We do not collect any personal information about you unless you
-          voluntarily provide it to us. However, you may be required to provide
-          certain personal information to us when you elect to use certain
-          products or services available on the Site. These may include: (a)
-          registering for an account on our Site; (b) entering a sweepstakes or
-          contest sponsored by us or one of our partners; (c) signing up for
-          special offers from selected third parties; (d) sending us an email
-          message; (e) submitting your credit card or other payment information
-          when ordering and purchasing products and services on our Site. To
-          wit, we will use your information for, but not limited to,
-          communicating with you in relation to services and/or products you
-          have requested from us. We also may gather additional personal or
-          non-personal information in the future.
-        </p>
-        <br />
-        <h2>Use of your Personal Information</h2>
-        <br />
-        <p>
-          PnutPresets collects and uses your personal information to operate its
-          website(s) and deliver the services you have requested. PnutPresets
-          may also use your personally identifiable information to inform you of
-          other products or services available from PnutPresets and its
-          affiliates.
-        </p>
-        <br />
-        <p>
-          Sharing Information with Third Parties PnutPresets does not sell, rent
-          or lease its customer lists to third parties.
-        </p>
-        <br />
-        <p>
-          PnutPresets may share data with trusted partners to help perform
-          statistical analysis, send you email or postal mail, provide customer
-          support, or arrange for deliveries. All such third parties are
-          prohibited from using your personal information except to provide
-          these services to PnutPresets, and they are required to maintain the
-          confidentiality of your information. PnutPresets may disclose your
-          personal information, without notice, if required to do so by law or
-          in the good faith belief that such action is necessary to: (a) conform
-          to the edicts of the law or comply with legal process served on
-          PnutPresets or the site; (b) protect and defend the rights or property
-          of PnutPresets; and/or (c) act under exigent circumstances to protect
-          the personal safety of users of PnutPresets, or the public.
-        </p>
-        <br />
-        <h2>Tracking User Behavior</h2>
-        <br />
-        <p>
-          PnutPresets may keep track of the websites and pages our users visit
-          within PnutPresets, in order to determine what PnutPresets services
-          are the most popular. This data is used to deliver customized content
-          and advertising within PnutPresets to customers whose behavior
-          indicates that they are interested in a particular subject area.
-        </p>
-        <br />
-        <ul>
-          <li className="bullet-point">Facebook Pixel Data Tracking </li>
-          <li className="bullet-point">Automatically Collected Information</li>
-        </ul>
-        <br />
-        <p>
-          Information about your computer hardware and software may be
-          automatically collected by PnutPresets. This information can include:
-          your IP address, browser type, domain names, access times and
-          referring website addresses. This information is used for the
-          operation of the service, to maintain quality of the service, and to
-          provide general statistics regarding use of the PnutPresets website.
-        </p>
-        <br />
-        <h2>Use of Cookies</h2>
-        <br />
-        <p>
-          The PnutPresets website may use “cookies” to help you personalize your
-          online experience. A cookie is a text file that is placed on your hard
-          disk by a web page server. Cookies cannot be used to run programs or
-          deliver viruses to your computer. Cookies are uniquely assigned to
-          you, and can only be read by a web server in the domain that issued
-          the cookie to you.
-        </p>
-        <br />
-        <p>
-          One of the primary purposes of cookies is to provide a convenience
-          feature to save you time. The purpose of a cookie is to tell the Web
-          server that you have returned to a specific page. For example, if you
-          personalize PnutPresets pages, or register with PnutPresets site or
-          services, a cookie helps PnutPresets to recall your specific
-          information on subsequent visits. This simplifies the process of
-          recording your personal information, such as billing addresses,
-          shipping addresses, and so on. When you return to the same PnutPresets
-          website, the information you previously provided can be retrieved, so
-          you can easily use the PnutPresets features that you customized.
-        </p>
-        <br />
-        <p>
-          You have the ability to accept or decline cookies. Most Web browsers
-          automatically accept cookies, but you can usually modify your browser
-          setting to decline cookies if you prefer. If you choose to decline
-          cookies, you may not be able to fully experience the interactive
-          features of the PnutPresets services or websites you visit.
-        </p>
-        <br />
-        <h2>Security of your Personal Information </h2>
-        <br />
-        <p>
-          PnutPresets secures your personal information from unauthorized
-          access, use, or disclosure. PnutPresets uses the following methods for
-          this purpose:
-        </p>
-        <br />
-        <ul>
-          <li className="bullet-point">– SSL Protocol</li>
-        </ul>
-        <br />
-        <p>
-          When personal information (such as a credit card number) is
-          transmitted to other websites, it is protected through the use of
-          encryption, such as the Secure Sockets Layer (SSL) protocol.
-        </p>
-        <br />
-        <p>
-          We strive to take appropriate security measures to protect against
-          unauthorized access to or alteration of your personal information.
-          Unfortunately, no data transmission over the Internet or any wireless
-          network can be guaranteed to be 100% secure. As a result, while we
-          strive to protect your personal information, you acknowledge that: (a)
-          there are security and privacy limitations inherent to the Internet
-          which are beyond our control; and (b) security, integrity, and privacy
-          of any and all information and data exchanged between you and us
-          through this Site cannot be guaranteed.
-        </p>
-        <br />
-        <h2>Children Under Thirteen</h2>
-        <br />
-        <p>
-          PnutPresets collects personally identifiable information from children
-          under the age of thirteen. PnutPresets collects this information for
-          the following reason(s):
-        </p>
+        <p>When using the Website, you agree to:</p>
         <br />
         <ul>
           <li className="bullet-point">
-            Tracking website user data & information during the signup process.{" "}
+            a. Comply with all applicable laws and regulations.{" "}
+          </li>
+          <li className="bullet-point">
+            b. Use the Website only for lawful purposes and in a manner that
+            does not infringe upon the rights of others.{" "}
+          </li>
+          <li className="bullet-point">
+            c. Refrain from transmitting any content that is unlawful, harmful,
+            defamatory, obscene, or otherwise objectionable.
+          </li>
+          <li className="bullet-point">
+            d. Maintain the confidentiality of any account credentials and not
+            share them with unauthorized individuals.
           </li>
         </ul>
         <br />
-        <p>
-          If you are under the age of thirteen, you must ask your parent or
-          guardian for permission to use this website. If you are a parent and
-          you have questions regarding our data collection practices, please
-          contact us using the information provided at the end of this Statement
-          of Privacy.
-        </p>
-        <br />
-        <h2>E-mail Communications</h2>
-        <br />
-        <p>
-          From time to time, PnutPresets may contact you via email for the
-          purpose of providing announcements, promotional offers, alerts,
-          confirmations, surveys, and/or other general communication. In order
-          to improve our Services, we may receive a notification when you open
-          an email from PnutPresets or click on a link therein.
-        </p>
-        <br />
-        <h2>External Data Storage Sites</h2>
-        <br />
-        <p>
-          We may store your data on servers provided by third party hosting
-          vendors with whom we have contracted.
-        </p>
 
-        <br />
-
-        <h2>Changes to this Statement</h2>
+        <h2>Third-Party Links</h2>
         <br />
         <p>
-          PnutPresets reserves the right to change this Privacy Policy from time
-          to time. We will notify you about significant changes in the way we
-          treat personal information by sending a notice to the primary email
-          address specified in your account, by placing a prominent notice on
-          our site, and/or by updating any privacy information on this page.
-          Your continued use of the Site and/or Services available through this
-          Site after such modifications will constitute your: (a) acknowledgment
-          of the modified Privacy Policy; and (b) agreement to abide and be
-          bound by that Policy.
+          The Website may contain links to third-party websites or services.
+          PnutPresets does not endorse or assume any responsibility for the
+          content, privacy practices, or actions of any third-party websites or
+          services. You access and use these third-party websites or services at
+          your own risk.
+        </p>
+        <br />
+        <h2>Disclaimer of Warranties </h2>
+        <br />
+        <p>
+          The Website and its content are provided on an "as-is" basis without
+          any warranties, express or implied. PnutPresets does not guarantee the
+          accuracy, reliability, or availability of the Website. You use the
+          Website at your own risk.
+        </p>
+        <br />
+        <h2>Limitation of Liability</h2>
+        <br />
+        <p>
+          In no event shall PnutPresets be liable for any direct, indirect,
+          incidental, consequential, or punitive damages arising out of or in
+          connection with the use of the Website. This includes, but is not
+          limited to, any errors or omissions in the content, loss of data, or
+          interruption of business.
         </p>
         <br />
 
-        <h2>Contact Information</h2>
+        <h2>Modification and Termination</h2>
         <br />
-
         <p>
-          PnutPresets welcomes your questions or comments regarding this
-          Statement of Privacy. If you believe that PnutPresets has not adhered
-          to this Statement, please contact PnutPresets here.
+          PnutPresets reserves the right to modify or terminate the Website or
+          any part thereof, at any time without notice. PnutPresets may also
+          revise these terms and conditions by posting an updated version on the
+          Website. Continued use of the Website after any such changes shall
+          constitute your consent to such changes.
+        </p>
+        <br />
+        <h2>Governing Law</h2>
+        <br />
+        <p>
+          These terms and conditions shall be governed by and construed in
+          accordance with the laws of [your jurisdiction]. Any legal action or
+          proceeding arising out of or related to these terms shall be brought
+          exclusively in the courts of [your jurisdiction].
         </p>
       </div>
     </div>
