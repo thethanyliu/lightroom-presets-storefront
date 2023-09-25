2a26acac621a8387557d40be04b5d4981c003c76
Ethan Liu <62630748+ethanliu666@users.noreply.github.com>
2023-09-25 14:39:29 -0400

    updated css

--- src/components/Home/FeaturedProduct.jsx
+++ src/components/Home/FeaturedProduct.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Product from "../Products/Product";
-import classes from "./FeaturedProduct.module.css"
+import styles from "./FeaturedProduct.module.css"
 
 const FeaturedProduct = ({ featuredProducts }) => {
   return (
-    <div className={classes.feature}>
-      <div className={classes.featureProducts}>
+    <div className={styles.feature}>
+      <div className={styles.featureProducts}>
         {featuredProducts.map((product, i) => {
           return <Product key={i} product={product} width={350} height={350} />;
         })}
